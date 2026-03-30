71fd714258b9d9e24a6ddc371334ec5e50b75430
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-03-30 12:24:30 -0300

    Add files via upload

---
+++ verificador.py
@@ -0,0 +1,8 @@
+usuario = input("Insira seu nome de Usuario: ")
+
+if usuario == "admim":
+    print("Acesso total liberado")
+else:
+    print("Acesso limitado concedido")
+    
+#---Ryan Nardini Pereira---
