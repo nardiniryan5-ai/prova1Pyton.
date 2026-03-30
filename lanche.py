6cc27a14ed8715e4400f79bea8258b403b0ffb04
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-03-30 12:24:03 -0300

    Add files via upload

---
+++ lanche.py
@@ -0,0 +1,10 @@
+valor = float(input("Qual valor do seu lanche?: "))
+
+if valor <= 10:
+    print("Preço Justo")
+elif valor <= 20:
+    print("Está ficando Caro")
+else:
+    print("Muito caro, melhor levar de casa!")
+
+#---Ryan Nardini Pereira---
