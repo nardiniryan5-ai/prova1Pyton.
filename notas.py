2ee6d788f37b9354af51d01add7f48b6712e5ebe
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-03-31 12:38:24 -0300

    Delete notas.py

--- notas.py
+++
@@ -1,5 +0,0 @@
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-nota3 = float(input("Digite a terceira nota: "))
-media= (nota1+nota3+nota2)/3
-print(media)
