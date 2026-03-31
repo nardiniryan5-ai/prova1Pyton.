51edceb537b2dfea50ffea9240e556ef200524fd
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-03-31 13:04:00 -0300

    Add files via upload

---
+++ simulador.py
@@ -0,0 +1,11 @@
+simulador = "sim"
+print("Você ainda está no jogo! 🎮")
+
+while simulador != "sim":
+ simulador = input("Deseja continuar jogando? (sim/nao)")
+ if simulador == "sim":
+    print("Você ainda está no jogo! 🎮")
+if simulador == "sim":
+    print("Game Over! 💀")
+
+  #---Ryan Nardini Pereira---
